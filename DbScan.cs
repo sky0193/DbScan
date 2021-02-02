4aab122bd6bdac9830bd8f6f994062a4839dfbcc
sky0193 <olena.bykova93@gmail.com>
2021-02-02 09:32:27 +0100

    chg: [F-1] renamings

--- DbScan.cs
+++ DbScan.cs
@@ -5,7 +5,7 @@ namespace DBScan.TestData
 {
     public class DbScan
     {
-        public int ClusterId{get;set;}
+        public int ClusterCount{get;set;}
         public float MaximumDistance{get;set;}
         public int MinimumPoints{get;set;}
 
@@ -14,10 +14,10 @@ namespace DBScan.TestData
         {
             MaximumDistance = maximumDistance;
             MinimumPoints = minimumPoints;
-            ClusterId = 0;
+            ClusterCount = 0;
         }
 
-        public void Cluster(List<DBScanPoint> setOfPoints){
+        public void ClusterPoints(List<DBScanPoint> setOfPoints){
             for(var i = 0; i < setOfPoints.Count; i++){
                 if(setOfPoints[i].Label == DBScanPointLabel.Unclassified)
                 {
@@ -28,22 +28,24 @@ namespace DBScan.TestData
                     }
                     else
                     {
-                        ClusterId ++;
-                        expandCluster(setOfPoints[i], neighborPoints, setOfPoints);
+                        ClusterCount ++;
+                        ExpandCluster(setOfPoints[i], neighborPoints, setOfPoints);
                     }
                 }
             }
         }
 
-        private void expandCluster(DBScanPoint point, List<DBScanPoint> neighborPoints, List<DBScanPoint> pointsToCluster)
+        private void ExpandCluster(DBScanPoint point,
+                                   List<DBScanPoint> neighborPoints,
+                                   List<DBScanPoint> pointsToCluster)
         {
-            point.ClusterId = ClusterId;
+            point.ClusterId = ClusterCount;
             for(int i = 0; i < neighborPoints.Count; i++)
             {
                 if(neighborPoints[i].Label == DBScanPointLabel.Unclassified)
                 {
                     neighborPoints[i].Label = DBScanPointLabel.Classified;
-                    neighborPoints[i].ClusterId = ClusterId;
+                    neighborPoints[i].ClusterId = ClusterCount;
                     List<DBScanPoint> neighborPointsOfNeighbor = GetRegion(pointsToCluster, neighborPoints[i]);
                     if(neighborPointsOfNeighbor.Count >= MinimumPoints)
                     {
@@ -54,7 +56,8 @@ namespace DBScan.TestData
             }
         }
 
-        private List<DBScanPoint> GetRegion(List<DBScanPoint> pointsToCluster, DBScanPoint GetRegionFromThisPoint)
+        private List<DBScanPoint> GetRegion(List<DBScanPoint> pointsToCluster,
+                                            DBScanPoint GetRegionFromThisPoint)
         {
             List<DBScanPoint> neighborPoints = new List<DBScanPoint>();
             double maximumDistanceSquared = MaximumDistance * MaximumDistance;
